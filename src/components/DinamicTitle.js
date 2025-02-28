5428fa72b8c73d31bd3cbdfa3372e8e4ce85e63f
leticia-awawdeh <leticiaawawdeh@gmail.com>
2025-02-27 21:53:53 -0300

    estilizando components

---
+++ src/components/DinamicTitle.js
@@ -0,0 +1,25 @@
+class DinamicTitle extends HTMLElement{
+    constructor(){
+        super();
+        
+        const shadow = this.attachShadow({mode: "open"});
+
+        //base do component
+        const componentRoot = document.createElement("h1");
+        componentRoot.textContent = 'Letícia'
+
+        //estilizar o component
+        const style = document.createElement('style');
+        style.textContent - `
+            h1{
+                color: red;
+            }
+        `;
+
+        //enviar para a shadow
+        shadow.appendChild(componentRoot);
+        shadow.appendChild(style);
+    }
+}
+
+customElements.define("dinamic-title", DinamicTitle);
